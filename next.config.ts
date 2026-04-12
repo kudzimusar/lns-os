0954af44ecb13bbf78033e42480f3ddeb65703fd
kudzimusar <kudzimusar@example.com>
2026-04-12 11:26:15 +0900

    Fix 404 issues locally by making basePath conditional

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/lns-os',
-  assetPrefix: '/lns-os',
+  basePath: isProd ? '/lns-os' : '',
+  assetPrefix: isProd ? '/lns-os' : '',
   images: {
     unoptimized: true,
   },
